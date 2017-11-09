4f201ec5848d0f48e413ca1b394ee6f5df34e72a
nemesgabor <nemesgabor@outlook.com>
2017-11-09 21:25:54 +0100

    16-215

--- src/app/recipes/recipes.service.ts
+++ src/app/recipes/recipes.service.ts
@@ -2,9 +2,11 @@ import { ShoppingListService } from './../shopping-list/shopping-list.service';
 import { Ingredient } from '../shared/ingredient.model';
 import { Injectable } from '@angular/core';
 import { Recipe } from './recipe.model';
+import { Subject } from 'rxjs/Subject';
 
 @Injectable()
 export class RecipeService {
+  recipesChanged = new Subject<Recipe[]>();
 
   private recipes: Recipe[] = [
     new Recipe(
@@ -38,5 +40,16 @@ export class RecipeService {
 
   getRecipe(index:number){
     return this.recipes[index];
+
+  }
+
+  addRecipe(recipe: Recipe){
+    this.recipes.push(recipe);
+    this.recipesChanged.next(this.recipes.slice());
+  }
+
+  updateRecipe(index: number, newRecipe: Recipe){
+    this.recipes[index] = newRecipe;
+    this.recipesChanged.next(this.recipes.slice());
   }
 }
